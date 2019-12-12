c2dcd4af9f5877273ba819b1ef72262022541991
OM-3-GA <55928680+OM-3-GA@users.noreply.github.com>
2019-12-12 18:55:59 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+var endDate = new Date("Nov 20, 2020 0:00:00").getTime();
+
+var timer = setInterval(function() {
+
+    let now = new Date().getTime();
+    let t = endDate - now;
+    
+    if (t >= 0) {
+    
+        let days = Math.floor(t / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        let secs = Math.floor((t % (1000 * 60)) / 1000);
+    
+        document.getElementById("timer-days").innerHTML = days + "<span class='label'>Days</span>";
+    
+        document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2) + "<span class='label'>Hours</span>";
+    
+        document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2) + "<span class='label'>Minutes</span>";
+    
+        document.getElementById("timer-secs").innerHTML = ("0"+secs).slice(-2) + "<span class='label'>Seconds</span>";
+		document.getElementById("bottom").innerHTML = "<br>Until Faith's Birthday";
+		
+    } else {
+        document.getElementById("timer").innerHTML = "Happy Birthday, Faith!";
+		document.getElementById("botton").innerHTML = "";
+    }
+    
+}, 1000);
+© 2019 GitHub, Inc.
